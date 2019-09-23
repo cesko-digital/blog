008f045bc3db1a460d215a8a0871f59e15bb48fb
horm <horak@horm.cz>
2019-09-23 22:34:30 +0200

    Add hover state for post card

--- src/components/post-card/styles.js
+++ src/components/post-card/styles.js
@@ -6,6 +6,10 @@ export const Container = styled.div`
   box-shadow: 0px 4px 14px rgba(10, 10, 10, 0.07);
   border-radius: 14px;
   height: 100%;
+  transition: all 250ms ease-in-out;
+  :hover {
+  box-shadow: 0px 8px 18px rgba(10, 10, 10, 0.12);
+  }
 `;
 
 export const Padding = styled.div`
