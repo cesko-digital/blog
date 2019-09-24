5c2aa20bf86f3c9352ac02ab65d589bb53b3bb5f
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2019-09-24 10:15:19 +0200

    Trivial site metadata tweaks

--- data/site-config.js
+++ data/site-config.js
@@ -1,17 +1,17 @@
 const config = {
-  siteTitle: "Blog >Česko.Digital", // Site title.
-  siteTitleAlt: "Blog >Česko.Digital", // Alternative site title for SEO.
+  siteTitle: ">Česko.Digital Blog", // Site title.
+  siteTitleAlt: ">Česko.Digital Blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo.svg", // Logo used for SEO and manifest.
   siteUrl: "https://blog.cesko.digital/", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Články ze světa komunity měnící Česko skrze jedničky a nuly...", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Skrz jedničky a nuly měníme svět k lepšímu.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   googleAnalyticsID: "", // GA tracking ID.
   postDefaultCategoryID: "Blog", // Default category for posts.
   dateFromFormat: "YYYY-MM-DD-HH-mm", // Date format used in the frontmatter.
   twitter: "CeskoDigital", // Optionally renders "Follow Me" in the UserInfo segment.
-  copyright: "Copyright © 2019. >Česko.Digital", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © >Česko.Digital", // Copyright string for the footer of the website and RSS feed.
 };
 
 // Validate
