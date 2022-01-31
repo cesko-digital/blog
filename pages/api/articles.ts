c9b6ad0962eb94fe7280babedcfcd20fd37c351e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-01-31 14:31:50 +0100

    Add article API endpoint

---
+++ pages/api/articles.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { BlogPost, getAllPosts, getPublicPostURL } from 'lib/post';
+
+interface PublicParams {
+  url?: string;
+  title: string;
+  date: string;
+  author: string;
+  slug: string;
+  cover: string;
+  tags: string[];
+  description: string;
+}
+
+function getPublicParamsForPost(post: BlogPost): PublicParams {
+  return {
+    url: getPublicPostURL(post),
+    title: post.title,
+    date: post.date,
+    author: post.authorId,
+    slug: post.slug,
+    cover: post.coverImageUrl,
+    tags: post.tags,
+    description: post.description,
+  };
+}
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const posts = getAllPosts('content/posts').map(getPublicParamsForPost);
+  response.setHeader('Content-Type', 'application/json');
+  response.status(200).send(JSON.stringify(posts, null, 2));
+}
