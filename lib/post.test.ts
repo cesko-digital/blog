d53f7b0a228ac5605ac6d4476da8884fec479587
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-01-31 14:20:24 +0100

    Load blog posts

---
+++ lib/post.test.ts
@@ -0,0 +1,45 @@
+import { decodeMetadata, Metadata, parsePath, readBlogPost } from './post';
+import { getFilesRecursively } from './utils';
+
+test('Decode post metadata', () => {
+  expect(
+    decodeMetadata({
+      title: 'Spolupráce s Česko.Digital',
+      author: 'marek',
+      cover: 'https://data.cesko.digital/img/clanek-pohyb-je-reseni/2.png',
+      date: '2022-01-10-01-25',
+      slug: 'pribeh-inkubacniho-procesu',
+      description: 'Příběh tříměsíčního designového procesu…',
+    })
+  ).toEqual<Metadata>({
+    title: 'Spolupráce s Česko.Digital',
+    authorId: 'marek',
+    coverImageUrl: 'https://data.cesko.digital/img/clanek-pohyb-je-reseni/2.png',
+    date: '2022-01-10-01-25',
+    slug: 'pribeh-inkubacniho-procesu',
+    description: 'Příběh tříměsíčního designového procesu…',
+    lang: 'cs',
+    tags: [],
+  });
+});
+
+test('Parse post path', () => {
+  expect(parsePath('2022-01-26-cist-digital-30.md')).toEqual([new Date('2022-01-26'), 'cist-digital-30']);
+  expect(() => parsePath('2021-2-90-cist-digital.md')).toThrow();
+  expect(() => parsePath('2021-2-cist-digital.md')).toThrow();
+  expect(() => parsePath('2021-2-12-.md')).toThrow();
+  expect(() => parsePath('2021-2-12-křeč.md')).toThrow();
+  expect(() => parsePath('2021-2-12-sleva-50%.md')).toThrow();
+});
+
+test('Parse all posts', () => {
+  const postPaths = getFilesRecursively('content/posts').filter((path) => path.endsWith('.md'));
+  for (const path of postPaths) {
+    try {
+      const _ = readBlogPost(path);
+    } catch (e) {
+      throw `Post fails to decode: ${path}: ${e}`;
+    }
+  }
+  console.log(`Successfully decoded ${postPaths.length} posts.`);
+});
---
+++ lib/post.ts
@@ -0,0 +1,77 @@
+import { array, decodeType, field, record, string, union } from 'typescript-json-decoder';
+import { getFilesRecursively, withDefault } from './utils';
+import matter from 'gray-matter';
+import fs from 'fs';
+
+export type Metadata = decodeType<typeof decodeMetadata>;
+
+export interface BlogPost extends Metadata {
+  body: string;
+}
+
+export const decodeMetadata = record({
+  title: string,
+  authorId: field('author', string),
+  coverImageUrl: field('cover', string),
+  date: string,
+  slug: string,
+  description: string,
+  lang: withDefault(union('cs', 'en'), 'cs'),
+  tags: withDefault(array(string), []),
+});
+
+/** Decode article from a standard frontmatter + body text file */
+export function decodeBlogPost(src: string, defaults: Record<string, any> = {}): BlogPost {
+  const { content, data } = matter(src);
+  const mergedMeta = { ...defaults, ...data };
+  return {
+    body: content,
+    ...decodeMetadata(mergedMeta),
+  };
+}
+
+/**
+ * Parse article path to get article date and slug
+ *
+ * The expected filename format is `2022-01-26-cist-digital-30.md`.
+ */
+export function parsePath(path: string): [Date, string] {
+  const filename = path.replace(/^.*[\\\/]/, '');
+  const matches = filename.match(/^(\d+-\d+-\d+)-(.*)\.md$/);
+
+  if (!matches || matches.length < 2) {
+    throw `Invalid file name format: ${filename}`;
+  }
+
+  const date = new Date(matches[1]);
+  const slug = matches[2];
+
+  if (isNaN(date.getTime())) {
+    throw `Invalid date ${date} in ${filename}`;
+  }
+
+  if (!slug.match(/^[a-z0-9-_]+$/i)) {
+    throw `Invalid slug “${slug}” in ${filename}`;
+  }
+
+  return [date, slug];
+}
+
+/**
+ * Read blog post from a file
+ *
+ * This also automatically fills in the correct default values for
+ * `slug` and `date`.
+ */
+export function readBlogPost(path: string): BlogPost {
+  const src = fs.readFileSync(path, { encoding: 'utf-8' });
+  const [date, slug] = parsePath(path);
+  return decodeBlogPost(src, { date, slug });
+}
+
+/** Read all blog posts under a given directory root */
+export function getAllArticles(root: string): BlogPost[] {
+  return getFilesRecursively(root)
+    .filter((path) => path.endsWith('.md'))
+    .map(readBlogPost);
+}
