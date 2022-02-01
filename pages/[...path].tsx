477942f336b84cfa869618e7a3e269b38c8b59d7
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-01 12:11:41 +0100

    Generate post pages

--- pages/[...path].tsx
+++ pages/[...path].tsx
@@ -1,32 +1,42 @@
+import { BlogPost } from "lib/post";
 import { siteData } from "lib/site-data";
 import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { ParsedUrlQuery } from "querystring";
 
 interface Props {
-  foo: string;
+  post: BlogPost;
 }
 
 interface QueryParams extends ParsedUrlQuery {
-  path: string;
+  path: string[];
 }
 
-const Post: NextPage<Props> = ({ foo }) => {
-  return <h1>{foo}</h1>;
+const Post: NextPage<Props> = ({ post }) => {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.description}</p>
+    </div>
+  );
 };
 
 export const getStaticProps: GetStaticProps<Props, QueryParams> = async (
   context
 ) => {
   const { path } = context.params!;
+  const mergedPath = "/" + path.join("/");
+  const post = siteData.posts.find((post) => post.path === mergedPath)!;
   return {
     props: {
-      foo: path,
+      post,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths<QueryParams> = async () => {
-  const paths = siteData.posts.map((post) => ({ params: { path: post.path } }));
+  const paths = siteData.posts.map((post) => ({
+    params: { path: post.path.split("/").slice(1) },
+  }));
   return {
     paths,
     fallback: false,
