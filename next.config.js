6fcaca641762593be4520a8848cdff41e2658dab
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-09 11:06:47 +0100

    Add a redirect for Netlify CMS

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/rss.xml',
-        destination: '/api/feed',
+        source: "/rss.xml",
+        destination: "/api/feed",
         permanent: false,
       },
-    ]
+      {
+        source: "/config.yml",
+        destination: "/admin/config.yml",
+        permanent: false,
+      },
+    ];
   },
-}
+};
