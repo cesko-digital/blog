6569a369c970dd638868b8db2fde69db56e9adfb
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-09 09:15:31 +0100

    Move Head into Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,42 @@
 import Footer from "./footer";
 import NavigationBar from "./navigation-bar";
+import { default as NextHead } from "next/head";
 
-const Layout: React.FC = (props) => {
+interface PageProps {
+  title: string;
+  description: string;
+  coverUrl?: string;
+}
+
+/** Main page layout with head, navigation and footer */
+const Layout: React.FC<PageProps> = (props) => {
   return (
-    <div className="main-wrapper">
-      <NavigationBar />
-      <div className="content-wrapper">{props.children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head {...props} />
+      <div className="main-wrapper">
+        <NavigationBar />
+        <div className="content-wrapper">{props.children}</div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+const Head: React.FC<PageProps> = (props) => {
+  const { title, description, coverUrl } = props;
+  return (
+    <NextHead>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={coverUrl} />
+      <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+      <script
+        defer
+        data-domain="blog.cesko.digital"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
+    </NextHead>
   );
 };
 
