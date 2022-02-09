f811278530ed95254caafcb64b71bc7f682a5ab1
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-09 09:58:03 +0100

    Add Google Tag Manager code

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,6 +36,15 @@ const Head: React.FC<PageProps> = (props) => {
         data-domain="blog.cesko.digital"
         src="https://plausible.io/js/plausible.js"
       ></script>
+      <script defer>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', 'GTM-KW2XL65');
+      `}
+      </script>
     </NextHead>
   );
 };
