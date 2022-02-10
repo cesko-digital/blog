75241faffdd604606db7485dcceabf511b4b38f9
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-10 16:16:47 +0100

    Improve MJML conversion endpoint

--- pages/api/mjml.ts
+++ pages/api/mjml.ts
@@ -6,11 +6,21 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const wantedPath = request.query.path as string;
-  const post = siteData.posts.find(
-    (p) => p.path === "/" + wantedPath.replace(".mjml", "")
-  )!;
+  const queryPath = request.query.path as string;
+
+  if (!queryPath) {
+    response.status(400).send("Missing “path” parameter.");
+    return;
+  }
+
+  const wantedPath = "/" + queryPath.replace(".mjml", "");
+  const post = siteData.posts.find((p) => p.path === wantedPath);
+  if (!post) {
+    response.status(404).send(`Source post not found for path: ${wantedPath}`);
+    return;
+  }
+
   const mjml = markdownToMJML(post.body);
-  response.setHeader("Content-Type", "text/plain");
+  response.setHeader("Content-Type", "text/plain; encoding=utf-8");
   response.status(200).send(renderMJML(mjml));
 }
