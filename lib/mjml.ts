f2ba8576d92e22b7e88bc5ce735c20b02f337a22
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-11 15:23:55 +0100

    Split blog post content to sections on images

--- lib/mjml.ts
+++ lib/mjml.ts
@@ -1,6 +1,9 @@
-import { markdownToHTML } from "./utils";
-import { BlogPost } from "./post";
 import fs from "fs";
+import { BlogPost } from "./post";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkHtml from "remark-html";
+import { Content } from "mdast";
 
 export type MJText = {
   type: "text";
@@ -27,7 +30,6 @@ export interface MJDocument {
 }
 
 export function blogPostToMJML(post: BlogPost): MJDocument {
-  const text = markdownToHTML(post.body);
   const styles = fs.readFileSync("content/mailing.css", { encoding: "utf-8" });
   return {
     styles: styles,
@@ -56,16 +58,75 @@ export function blogPostToMJML(post: BlogPost): MJDocument {
         ],
       },
       /* Post body */
-      {
+      ...parseMarkdownToSections(post.body),
+    ],
+  };
+}
+
+/**
+ * Parse Markdown to sections with HTML content
+ *
+ * Theoretically we could just produce one big section with all the HTML content
+ * inside. Practically, images in such section would not be responsive 💩 So let’s
+ * break the HTML content by block images and place those in separate sections using
+ * <mj-image>, that way they should be responsive.
+ *
+ * The algorithm is to look for top-level paragraphs that only have one single image
+ * as a sole child. These images should break the document into separate <mj-section>s.
+ */
+export function parseMarkdownToSections(markdown: string): MJSection[] {
+  const parser = unified().use(remarkParse);
+  const renderer = unified().use(remarkHtml);
+  const root = parser.parse(markdown);
+
+  let accumulatedContent: Content[] = [];
+  let sections: MJSection[] = [];
+
+  const wrap = (children: Content[]) => ({
+    type: "root",
+    children,
+  });
+
+  const flushAccumulatedContent = () => {
+    if (accumulatedContent.length > 0) {
+      const html = renderer.stringify(
+        wrap(accumulatedContent)
+      ) as any as string;
+      sections.push({
         content: [
           {
             type: "text",
-            text,
+            text: html,
           },
         ],
-      },
-    ],
+      });
+    }
   };
+
+  for (const node of root.children) {
+    if (
+      node.type === "paragraph" &&
+      node.children.length === 1 &&
+      node.children[0].type === "image"
+    ) {
+      const img = node.children[0];
+      flushAccumulatedContent();
+      sections.push({
+        content: [
+          {
+            type: "image",
+            src: img.url,
+          },
+        ],
+      });
+    } else {
+      accumulatedContent.push(node);
+    }
+  }
+
+  flushAccumulatedContent();
+
+  return sections;
 }
 
 export function renderMJML(document: MJDocument): string {
