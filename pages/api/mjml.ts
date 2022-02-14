25b8bfb3d7040d882b2e0fbe3f158490dfe5555d
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-14 14:29:07 +0100

    Add endpoint to export blog posts in newsletter form (#256)
    * Add code to parse blog posts into mailing sections
    
    * Use simpler MJML conversion route; add “.mjml” route shortcut
    
    * Improve MJML conversion endpoint
    
    * Improve MJML rendering
    
    * Remove unnecessary deps
    
    * Split blog post content to sections on images
    
    * Add HTML output
    
    * Add unsubscribe footer
    
    * Add endpoint caching
    
    * Fix file naming
    
    * Disallow robots from the MJML API endpoint
    
    * Fix accumulated content not being cleared

---
+++ pages/api/mjml.ts
@@ -0,0 +1,45 @@
+import { blogPostToMJML, renderMJML } from "lib/mjml";
+import { siteData } from "lib/site-data";
+import type { NextApiRequest, NextApiResponse } from "next";
+import mjml2html from "mjml";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const queryPath = request.query.path as string;
+  const wantsHtml = !!request.query.html;
+  const extension = wantsHtml ? ".newsletter" : ".mjml";
+
+  if (!queryPath) {
+    response.status(400).send("Missing “path” parameter.");
+    return;
+  }
+
+  const wantedPath = "/" + queryPath.replace(extension, "");
+  const post = siteData.posts.find((p) => p.path === wantedPath);
+  if (!post) {
+    response.status(404).send(`Source post not found for path: ${wantedPath}`);
+    return;
+  }
+
+  // Let’s keep the cache fairly short-lived if the editor needs to
+  // tune the content and go back-and-forth several times.
+  response.setHeader(
+    "Cache-Control",
+    "max-age=0, s-maxage=60, stale-while-revalidate=60"
+  );
+
+  // Let’s make sure these versions don’t affect our SEO.
+  response.setHeader("X-Robots-Tag", "noindex, nofollow");
+
+  const mjml = renderMJML(blogPostToMJML(post));
+
+  if (wantsHtml) {
+    response.setHeader("Content-Type", "text/html; encoding=utf-8");
+    response.status(200).send(mjml2html(mjml).html);
+  } else {
+    response.setHeader("Content-Type", "text/plain; encoding=utf-8");
+    response.status(200).send(mjml);
+  }
+}
