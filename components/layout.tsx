03fd5b4fae0e918ae514ef334a99bfdd11cfee20
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-15 15:44:24 +0100

    Make Analytics great again

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,6 +14,19 @@ const Layout: React.FC<PageProps> = (props) => {
   return (
     <>
       <Head {...props} />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-140227366-2" />
+      <Script id="google-analytcs">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-140227366-2');
+        `}
+      </Script>
+      <Script
+        data-domain="blog.cesko.digital"
+        src="https://plausible.io/js/plausible.js"
+      />
       <div className="main-wrapper">
         <NavigationBar />
         <div className="content-wrapper">{props.children}</div>
@@ -32,20 +45,6 @@ const Head: React.FC<PageProps> = (props) => {
       <meta property="og:title" content={title} />
       <meta property="og:image" content={coverUrl} />
       <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-      <script
-        defer
-        data-domain="blog.cesko.digital"
-        src="https://plausible.io/js/plausible.js"
-      ></script>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', 'GTM-KW2XL65');
-      `}
-      </Script>
     </NextHead>
   );
 };
