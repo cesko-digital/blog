826db0d0af9c79074237f6aa88356f1987d1e419
Tomáš Znamenáček <zoul@zoul.cz>
2023-02-16 15:33:53 +0100

    Add basic Playwright infrastructure & endpoint tests

---
+++ tests/feeds.e2e.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("Article endpoint works", async ({ page }) => {
+  const response = await page.request.get("/api/articles");
+  await expect(response).toBeOK();
+  expect(await response.json()).toBeTruthy();
+});
+
+test("RSS feed endpoint works", async ({ page }) => {
+  const response = await page.request.get("/api/feed");
+  await expect(response).toBeOK();
+});
