137627ca58ff2577e83e16661e776437ffbc6838
Tomáš Znamenáček <zoul@zoul.cz>
2023-02-17 10:56:49 +0100

    Add a fixme E2E test for the 404 page

--- tests/basic.e2e.ts
+++ tests/basic.e2e.ts
@@ -4,3 +4,10 @@ test("Sanity check", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/Blog/);
 });
+
+// https://github.com/cesko-digital/blog/issues/473
+test.fixme("Not found page", async ({ page }) => {
+  const response = await page.request.get("/does_not_exist");
+  expect(response.status()).toBe(404);
+  expect(await response.text()).not.toMatch(/NEXT_NOT_FOUND/);
+});
