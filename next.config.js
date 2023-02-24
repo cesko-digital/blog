a2748254a9c0d2c184e693f91392769a2b6f4644
Tomáš Znamenáček <zoul@zoul.cz>
2023-02-24 08:30:34 +0100

    Simplify RSS feed routing

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/rss.xml",
-        destination: "/api/feed",
-        permanent: false,
-      },
-      {
         source: "/config.yml",
         destination: "/admin/config.yml",
         permanent: false,
