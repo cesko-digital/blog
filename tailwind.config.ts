25fdcab70e825087ca38c91acf2e0246aea616c6
Tomáš Znamenáček <zoul@zoul.cz>
2024-05-22 08:42:42 +0200

    Add Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        content: "1200px",
+      },
+      colors: {
+        pebble: "#F9F9F9",
+        it: "#0000FF",
+        violet: "CCCCFF",
+        yellow: "#FFF6A3",
+        plum: "#080831",
+        gray: "#EDEDEF",
+        gravel: "#A9A9B1",
+        asphalt: "#47475B",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
