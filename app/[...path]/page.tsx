8ba6603f4efd158de3bed6f2ab860a05c1a9db91
Tomáš Znamenáček <zoul@zoul.cz>
2024-05-23 10:39:55 +0200

    Move back button styles to Tailwind

--- app/[...path]/page.tsx
+++ app/[...path]/page.tsx
@@ -10,6 +10,9 @@ import {
 import { markdownToHTML } from "shared/utils";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import { Fragment } from "react";
 
 type Params = {
   path: string[];
@@ -46,31 +49,48 @@ const Post = ({ params }: Props) => {
 const PostBody = ({ post, author }: { post: BlogPost; author: Author }) => {
   const formatDate = (stamp: string) =>
     new Date(stamp).toLocaleDateString("cs-CZ", { dateStyle: "medium" });
+
+  const BackButton = () => (
+    <Link
+      className="inline-block rounded-md bg-it px-4 py-3 text-white"
+      href="/"
+    >
+      <div className="flex flex-row gap-2">
+        <Image src="/arrow-light.svg" width={24} height={24} alt="" />
+        Zpět na všechny články
+      </div>
+    </Link>
+  );
+
+  const PostMetadata = () => (
+    <Fragment>
+      <div className="flex flex-row gap-2 -mb-2">
+        {formatDate(post.date)}
+        <a href={`mailto:${author.email}`}>{author.name}</a>
+      </div>
+
+      <h1 className="typo-title">{post.title}</h1>
+      <p>{post.description}</p>
+    </Fragment>
+  );
+
   return (
     <div
       className="border-2 border-gray rounded-xl overflow-clip"
       lang={post.lang}
     >
+      {/* TBD: Replace with next/image */}
       <img src={post.coverImageUrl} alt="" loading="lazy" width="100%" />
 
       <div className="flex flex-col gap-4 p-5">
-        <div className="flex flex-row gap-2 -mb-2">
-          {formatDate(post.date)}
-          <a href={`mailto:${author.email}`}>{author.name}</a>
-        </div>
-
-        <h1 className="typo-title">{post.title}</h1>
-        <p>{post.description}</p>
-
+        <PostMetadata />
         <div
           className="post-content"
           dangerouslySetInnerHTML={{ __html: markdownToHTML(post.body) }}
         />
-
-        <a className="post-button" href={"/"}>
-          <img src="/arrow-light.svg" alt="" />
-          Zpět na všechny články
-        </a>
+        <div className="p-4 pl-0 pt-6 text-center">
+          <BackButton />
+        </div>
       </div>
     </div>
   );
